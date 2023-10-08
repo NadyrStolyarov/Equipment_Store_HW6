83e913babe23bf897cbe7b3d4b61323af2cdae0a
Надыр <131809409+NadyrStolyarov@users.noreply.github.com>
2023-10-08 15:52:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Laptop laptop1 = new Laptop ("hp_pailion_g6", 16, 500, "Windows", "black", 16);
         Laptop laptop2 = new Laptop ("hp_galaxy_x64", 8, 500, "Windows", "white", 15.6);
-        Laptop laptop3 = new Laptop ("samsung_a52", 8, 300, "Not OS", "black", 16);
-        Laptop laptop4 = new Laptop ("lenovo_gtx76", 16, 700, "Windows", "white", 17.6);
+        Laptop laptop3 = new Laptop ("samsung_a52", 8, 300, "Windows", "black", 16);
+        Laptop laptop4 = new Laptop ("lenovo_gtx76", 32, 700, "Windows", "white", 17.6);
         Laptop laptop5 = new Laptop ("macbook_14pro", 8, 1000, "ios", "white", 16);
         Set<Laptop> unicLaptop = new HashSet<Laptop> ();
         unicLaptop.add (laptop1);
@@ -25,19 +25,17 @@ public class Main {
         mapCrit.put (4, "цвет");
         mapCrit.put (5, "диагональ");
         Scanner sc = new Scanner (System.in);
-        System.out.println ("Введите желаемые характеристики: 1. объем оперативной памяти: ");
+        System.out.println ("Введите желаемые характеристики: 1. минимальный объем оперативной памяти: ");
         int ramUser = sc.nextInt ();
-        System.out.println ("объем накопителя: ");
+        System.out.println ("минимальный объем накопителя: ");
         int storUser = sc.nextInt ();
-        System.out.println ("диагональ");
+        System.out.println ("минимальную диагональ");
         double digUser = sc.nextDouble ();
         for (Laptop lap : unicLaptop) {
             if ((lap.getRam () >= ramUser) && (lap.getStorageCap () >= storUser) && lap.getDiagonal () >= digUser) {
                 System.out.println (lap.toString ());
             }
-            else;
-            System.out.println("Подходящих параметров нету, Просим прощения.");
-            break;
+           
         }
         sc.close ();
     }
